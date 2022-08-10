f5fbed783d1e2a1b8d555dab65627136d40bb74c
Aidan Trimmer <aidan@trimmerfamily.com>
2022-08-10 12:55:09 -0400

    formatting code

--- ask.js
+++ ask.js
@@ -1,40 +1,40 @@
 // authenticates you with the API standard library
 // type `await lib.` to display API autocomplete
-const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+const lib = require("lib")({ token: process.env.STDLIB_SECRET_TOKEN });
 
-let question = context.params.event.content.split(' ').slice(1).join(' ')
+let question = context.params.event.content.split(" ").slice(1).join(" ");
 
 if (!question) {
-  await lib.discord.channels['@0.3.0'].messages.create({
+  await lib.discord.channels["@0.3.0"].messages.create({
     channel_id: `${context.params.event.channel_id}`,
     content: [
       `Whoops! Looks like you didn't use the prefix command properly`,
       `You must use the prefix command followed by a question`,
-      '',
-      'To view a list of questions I can answer use the command **!help**',
-      '**For example: !ask How to change bot status?**'
-    ].join('\n')
+      "",
+      "To view a list of questions I can answer use the command **!help**",
+      "**For example: !ask How to change bot status?**",
+    ].join("\n"),
   });
-  return
+  return;
 }
 
-let frequencyQuery = await lib.googlesheets.query['@0.3.0'].distinct({
+let frequencyQuery = await lib.googlesheets.query["@0.3.0"].distinct({
   range: `A:C`,
   bounds: `FIRST_EMPTY_ROW`,
   where: [
     {
-      'Question__contains': question,
+      Question__contains: question,
     },
   ],
   field: `Frequency`,
 });
 
-let frequency = parseInt(frequencyQuery.distinct.values[0])
-console.log('frequency ' + parseInt(frequencyQuery.distinct.values[0]));
-if (frequencyQuery.distinct.values[0] === ''){
-  await lib.googlesheets.query['@0.3.0'].update({
+let frequency = parseInt(frequencyQuery.distinct.values[0]);
+console.log("frequency " + parseInt(frequencyQuery.distinct.values[0]));
+if (frequencyQuery.distinct.values[0] === "") {
+  await lib.googlesheets.query["@0.3.0"].update({
     range: `A:C`,
-    bounds: 'FIRST_EMPTY_ROW',
+    bounds: "FIRST_EMPTY_ROW",
     where: [
       {
         Question__contains: question,
@@ -45,79 +45,84 @@ if (frequencyQuery.distinct.values[0] === ''){
     },
   });
 } else {
-await lib.googlesheets.query['@0.3.0'].update({
+  await lib.googlesheets.query["@0.3.0"].update({
+    range: `A:C`,
+    bounds: "FIRST_EMPTY_ROW",
+    where: [
+      {
+        Question__contains: question,
+      },
+    ],
+    fields: {
+      Frequency: frequency + 1,
+    },
+  });
+}
+
+let answer = await lib.googlesheets.query["@0.3.0"].select({
   range: `A:C`,
-  bounds: 'FIRST_EMPTY_ROW',
+  bounds: "FIRST_EMPTY_ROW",
   where: [
     {
       Question__contains: question,
     },
   ],
-  fields: {
-    Frequency: frequency + 1,
+  limit: {
+    count: 0,
+    offset: 0,
   },
 });
-}
-
-let answer = await lib.googlesheets.query['@0.3.0'].select({
-  range: `A:C`,
-  bounds: 'FIRST_EMPTY_ROW',
-  where: [
-    {
-      'Question__contains': question
-    }
-  ],
-  limit: {
-    'count': 0,
-    'offset': 0
-  }
-});
-
 if (!answer.rows.length) {
-  await lib.googlesheets.query['@0.3.0'].insert({
+  await lib.googlesheets.query["@0.3.0"].insert({
     range: `E:E`,
     fieldsets: [
       {
-        'Missing': question
-      }
-    ]
+        Missing: question,
+      },
+    ],
   });
   console.log("working");
 }
 
 if (!answer.rows.length) {
-  await lib.discord.channels['@0.3.0'].messages.create({
+  await lib.discord.channels["@0.3.0"].messages.create({
     channel_id: `${context.params.event.channel_id}`,
     content: [
-      `I couldn't find any answers matching the query **${question}**.`
-    ].join('\n')
+      `I couldn't find any answers matching the query **${question}**.`,
+    ].join("\n"),
   });
   return;
-} 
-console.log('Answer ' + answer.rows[0].fields.Question + ' ' + answer.rows[0].fields.Answer)
+}
+
+console.log(
+  "Answer " +
+    answer.rows[0].fields.Question +
+    " " +
+    answer.rows[0].fields.Answer
+);
 console.log(answer.rows.length);
 for (let i = 0; i < answer.rows.length; i++) {
-    if (i >= 4) {
-      await lib.discord.channels['@0.3.0'].messages.create({
-        channel_id: `${context.params.event.channel_id}`,
-        content: [
-          `There are too many with **${question}** in the name please try and be more specific.`
-        ].join('\n')
-      });
-      return;
-    } else {
-    await lib.discord.channels['@0.3.0'].messages.create({
+  if (i >= 4) {
+    await lib.discord.channels["@0.3.0"].messages.create({
       channel_id: `${context.params.event.channel_id}`,
-      content:'',
-      embed: {
-        color: 0x00AA00,
-        fields: [{
-          name:  answer.rows[i].fields.Question,
-          value:  answer.rows[i].fields.Answer
-        }]
-      }
+      content: [
+        `There are too many with **${question}** in the name please try and be more specific.`,
+      ].join("\n"),
     });
-    }
+    return;
+  } else {
+    await lib.discord.channels["@0.3.0"].messages.create({
+      channel_id: `${context.params.event.channel_id}`,
+      content: "",
+      embed: {
+        color: 0x00aa00,
+        fields: [
+          {
+            name: answer.rows[i].fields.Question,
+            value: answer.rows[i].fields.Answer,
+          },
+        ],
+      },
+    });
   }
-
-  
+}
